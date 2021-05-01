6c196bef78f2b7e7b0495455530429b7b3520ee3
quillasp <saralfddin93@yahoo.fr>
2021-05-01 17:56:28 +0200

    Cleaning in the imports

--- prank-bot/src/main/java/ch/heigvd/res/prankbot/config/ConfigManager.java
+++ prank-bot/src/main/java/ch/heigvd/res/prankbot/config/ConfigManager.java
@@ -1,27 +1,24 @@
 package ch.heigvd.res.prankbot.config;
 
 
+import ch.heigvd.res.prankbot.Groupe;
 import ch.heigvd.res.prankbot.Personne;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 public class ConfigManager {
-    String smtpServerAdress;
-    int smtpServerPort;
-    int numberOfGroups;
-    private final List<Personne> victimes;
+    private final String smtpServerAdress;
+    private final int smtpServerPort;
+    private final int numberOfGroups;
+    private final Groupe victimes;
 
-    public ConfigManager (String configFile, String victimesFile) throws IOException {
+    public ConfigManager (String configFile, String victimesFile) {
         Properties prop = readPropertiesFile(configFile);
         smtpServerAdress = prop.getProperty("smtpServerAdress");
         smtpServerPort = Integer.parseInt(prop.getProperty("smtpServerPort"));
@@ -29,7 +26,7 @@ public class ConfigManager {
         victimes = getVictimes(victimesFile, numberOfGroups);
     }
 
-    private Properties readPropertiesFile(String file) throws IOException {
+    private Properties readPropertiesFile(String file) {
         Properties prop = null;
         try (Reader reader = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8)) {
             prop = new Properties();
@@ -41,8 +38,10 @@ public class ConfigManager {
         return prop;
     }
 
-    private List<Personne> getVictimes (String file, int number) {
-        List<Personne> victimes = new ArrayList<>();
+    private Groupe getVictimes (String file, int number) {
+        Personne emetteur;
+        Groupe victimes = null;
+
         JSONParser jsonParser = new JSONParser();
 
         try (FileReader reader = new FileReader(file)) {
@@ -50,23 +49,48 @@ public class ConfigManager {
 
             JSONObject jsonObject = (JSONObject) obj;
 
-            JSONArray victimesList = (JSONArray) jsonObject.get("victimes");
+            JSONArray listVictimes = (JSONArray) jsonObject.get("victimes");
 
-            int counter = 0;
-            Iterator<JSONObject> iterator = victimesList.iterator();
-            while (iterator.hasNext() && counter < number) {
-                JSONObject truc = iterator.next();
-                if (truc.containsKey("name")) {
-                    victimes.add(new Personne((String) truc.get("mail"), (String) truc.get("name")));
+            if (number > listVictimes.size())
+                throw new IllegalArgumentException("number of victims must be lesser " +
+                        "than the size of the victimes file");
+
+            int numberEmetteur = getRandomNumberInRange(1, listVictimes.size());
+            JSONObject jsonEmetteur = (JSONObject) listVictimes.get(numberEmetteur);
+            if (jsonEmetteur.containsKey("name")) {
+                emetteur = new Personne((String) jsonEmetteur.get("mail"), (String) jsonEmetteur.get("name"));
+            } else {
+                emetteur = new Personne((String) jsonEmetteur.get("mail"));
+            }
+            listVictimes.remove(numberEmetteur);
+
+            victimes = new Groupe(emetteur);
+
+            for (int i = 0; i < number - 1; ++i) {
+                int randVictime = getRandomNumberInRange(1, listVictimes.size());
+                JSONObject jsonVictime = (JSONObject) listVictimes.get(randVictime);
+                if (jsonVictime.containsKey("name")) {
+                    victimes.addDestinataire(new Personne((String) jsonVictime.get("mail"),
+                            (String) jsonVictime.get("name")));
                 } else {
-                    victimes.add(new Personne((String) truc.get("mail")));
+                    victimes.addDestinataire(new Personne((String) jsonVictime.get("mail")));
                 }
+                listVictimes.remove(randVictime);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         return victimes;
+    }
+
+    private int getRandomNumberInRange(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt(max - min) + min;
     }
 
     public String getSmtpServerAdress () {
@@ -81,7 +105,7 @@ public class ConfigManager {
         return numberOfGroups;
     }
 
-    public List<Personne> getVictimes () {
+    public Groupe getVictimes () {
         return victimes;
     }
 }
