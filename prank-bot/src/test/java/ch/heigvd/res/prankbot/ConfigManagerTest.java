904bda1b9d7e779686bbf1c0bb4b9b5ff06cf8a5
david-pellissier <dav.pellissier@gmail.com>
2021-05-01 21:05:57 +0200

    PrankGenerator

--- prank-bot/src/test/java/ch/heigvd/res/prankbot/ConfigManagerTest.java
+++ prank-bot/src/test/java/ch/heigvd/res/prankbot/ConfigManagerTest.java
@@ -4,8 +4,6 @@ import ch.heigvd.res.prankbot.config.ConfigManager;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
-
 public class ConfigManagerTest {
     @Test
     public void InitConfigManager() {
