70050175c7b59b9436498ff85f395bc7be865dab
quillasp <saralfddin93@yahoo.fr>
2021-05-01 16:55:39 +0200

    Addition of the victim list from json file support

--- prank-bot/src/main/java/ch/heigvd/res/prankbot/config/ConfigManager.java
+++ prank-bot/src/main/java/ch/heigvd/res/prankbot/config/ConfigManager.java
@@ -3,19 +3,30 @@ package ch.heigvd.res.prankbot.config;
 
 import ch.heigvd.res.prankbot.Personne;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
 public class ConfigManager {
-    private List<Personne> victimes;
+    String smtpServerAdress;
+    int smtpServerPort;
+    int numberOfGroups;
+    private final List<Personne> victimes;
 
     public ConfigManager (String configFile, String victimesFile) throws IOException {
         Properties prop = readPropertiesFile(configFile);
-        String smtpServerAdress = prop.getProperty("smtpServerAdress");
-        int smtpServerPort = Integer.parseInt(prop.getProperty("smtpServerPort"));
-        int numberOfGroups = Integer.parseInt(prop.getProperty("numberOfGroups"));
+        smtpServerAdress = prop.getProperty("smtpServerAdress");
+        smtpServerPort = Integer.parseInt(prop.getProperty("smtpServerPort"));
+        numberOfGroups = Integer.parseInt(prop.getProperty("numberOfGroups"));
+        victimes = getVictimes(victimesFile, numberOfGroups);
     }
 
     private Properties readPropertiesFile(String file) throws IOException {
@@ -28,5 +39,49 @@ public class ConfigManager {
         }
 
         return prop;
+    }
+
+    private List<Personne> getVictimes (String file, int number) {
+        List<Personne> victimes = new ArrayList<>();
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader(file)) {
+            Object obj = jsonParser.parse(reader);
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+            JSONArray victimesList = (JSONArray) jsonObject.get("victimes");
+
+            int counter = 0;
+            Iterator<JSONObject> iterator = victimesList.iterator();
+            while (iterator.hasNext() && counter < number) {
+                JSONObject truc = iterator.next();
+                if (truc.containsKey("name")) {
+                    victimes.add(new Personne((String) truc.get("mail"), (String) truc.get("name")));
+                } else {
+                    victimes.add(new Personne((String) truc.get("mail")));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return victimes;
+    }
+
+    public String getSmtpServerAdress () {
+        return smtpServerAdress;
+    }
+
+    public int getSmtpServerPort () {
+        return smtpServerPort;
+    }
+
+    public int getNumberOfGroups () {
+        return numberOfGroups;
+    }
+
+    public List<Personne> getVictimes () {
+        return victimes;
     }
 }
